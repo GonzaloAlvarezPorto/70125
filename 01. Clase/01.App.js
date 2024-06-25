2aa333adf1ab7cfdcf910f7e14cad095d7cb6c5e
GonzaloAlvarezPorto <gonzaalvarezporto@gmail.com>
2024-06-25 20:35:25 -0300

    Clase 0

---
+++ 01. Clase/01.App.js
@@ -0,0 +1,8 @@
+//En la consola node 01.App.js devuelve el texto
+console.log("Hola Coders");
+
+console.warn("La concha del mono");
+
+console.error("Huracán amargo");
+
+
---
+++ 01. Clase/02.App.js
@@ -0,0 +1,22 @@
+/*Variables
+
+let -> puede cambiar
+const -> constante
+var -> puede cambiar y es global -----> NO SE USA MÁS
+
+let nombre = "Gonzalo";
+console.log(nombre);
+
+nombre = "Mateo"; //Reasignación
+console.log(nombre);
+
+let array = ["Gonzalo", "Mateo", "Gisela"];
+console.log(array);
+
+let edad = 36;
+console.log(edad);
+
+edad++;
+console.log(edad);
+*/
+
