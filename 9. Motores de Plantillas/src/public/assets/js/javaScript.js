5b2d1236df0ec3b8928293336a8eb9dfd70aaa7d
GonzaloAlvarezPorto <gonzaalvarezporto@gmail.com>
2024-07-31 01:54:00 -0300

    Clase 9

---
+++ 9. Motores de Plantillas/src/public/assets/js/javaScript.js
@@ -0,0 +1,1 @@
+console.log('Bienvendios al home')
---
+++ 9. Motores de Plantillas/src/routes/views.router.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+
+const router = Router();
+
+const users = [
+    { id: '1', full_name: 'user example 1', email: 'user1@gmail.com' },
+    { id: '2', full_name: 'user example 2', email: 'user2@gmail.com' },
+    { id: '3', full_name: 'user example 3', email: 'user3@gmail.com' },
+]
+
+router.get('/', (req, res) => {
+    const userLogin = {
+        full_name: 'Gonzalito',
+        role: 'admin'
+    }
+
+    res.render('index', {
+        user: userLogin,
+        isAdmin: userLogin.role === 'admin',
+        users,
+        title: 'Paykuna',
+        styles: "styles.css",
+        isMenu: true
+    })
+})
+
+router.get('/products', (req, res) => {
+    const userLogin = {
+        full_name: 'Gonzalito',
+        role: 'admin'
+    }
+    
+    res.render('products', {
+        user: userLogin,
+        isAdmin: userLogin.role === 'admin',
+        users,
+        title: 'Paykuna',
+        styles: "styles.css",
+        isMenu: false
+    })
+})
+
+module.exports = router;
