0bd1f42816c5fc36c37bf50e08a5dbb7e8529232
GonzaloAlvarezPorto <gonzaalvarezporto@gmail.com>
2024-08-03 17:49:11 -0300

    Clase 10

---
+++ 10. Websockets/src/routes/views.router.js
@@ -0,0 +1,48 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.use('/', (req, res) => {
+    res.render('index', {})
+})
+
+const users = [
+    { id: '1', full_name: 'user example 1', email: 'user1@gmail.com' },
+    { id: '2', full_name: 'user example 2', email: 'user2@gmail.com' },
+    { id: '3', full_name: 'user example 3', email: 'user3@gmail.com' },
+]
+
+
+router.get('/users', (req, res) => {
+    const userLogin = {
+        full_name: 'Gonzalito',
+        role: 'admin'
+    }
+
+    res.render('users', {
+        user: userLogin,
+        isAdmin: userLogin.role === 'admin',
+        users,
+        title: 'Paykuna',
+        styles: "styles.css",
+        isMenu: true
+    })
+})
+
+router.get('/products', (req, res) => {
+    const userLogin = {
+        full_name: 'Gonzalito',
+        role: 'admin'
+    }
+    
+    res.render('products', {
+        user: userLogin,
+        isAdmin: userLogin.role === 'admin',
+        users,
+        title: 'Paykuna',
+        styles: "styles.css",
+        isMenu: false
+    })
+})
+
+module.exports = router;
